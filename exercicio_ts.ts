42cf1e7f7536910869beccc8220c6de78b316fa6
RicardoLopesFV <ricardolopesprog@gmail.com>
2024-12-14 11:37:57 -0300

    first commit

---
+++ exercicio_ts.ts
@@ -0,0 +1,14 @@
+// Módulo de Typescript
+
+// Função de multiplicação
+const multiplicacao = (a: number, b: number): number => {
+  return a * b
+}
+
+// Função de saudação
+const saudacao = (nome: string): string => {
+  return `Olá ${nome}!`
+}
+
+console.log(multiplicacao(2, 3))
+console.log(saudacao('Ricardo'))
